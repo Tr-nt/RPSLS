fbc7086fddd05d3562fc1cbab90589790445ff78
Trent Powell <tdshanepowell@gmail.com>
2023-02-08 00:38:24 -0500

    Created AI class

---
+++ AI.py
@@ -0,0 +1,10 @@
+from player import Player
+import random
+class AI(Player):
+    def __init__():
+        super().__init__()
+
+    def choose_gesture(self):
+        n = random.randint(0,2)
+        print(self.gesture_list[n])
+        
